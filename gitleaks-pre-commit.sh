52f3b81469346e9cf9321f00ce1c926be45f003e
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-14 20:31:01 +0000

    fixed link

--- gitleaks-pre-commit.sh
+++ gitleaks-pre-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sSL https://github.com/1minEpowMinX/gitleaks-pre-commit-hook/pre-commit.py -o pre-commit.py
+curl -sSL https://raw.githubusercontent.com/1minEpowMinX/gitleaks-pre-commit-hook/main/pre-commmit.py -o pre-commit.py
 sudo chmod +x pre-commit.py
 mv pre-commit.py .git/hooks/pre-commit.py
