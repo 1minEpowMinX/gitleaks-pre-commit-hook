363a592079ec2941240dc3b2a67361a3b74353e7
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-14 21:55:27 +0000

    add messages

--- gitleaks-pre-commit.sh
+++ gitleaks-pre-commit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 curl -sSL https://raw.githubusercontent.com/1minEpowMinX/gitleaks-pre-commit-hook/main/pre-commmit.py -o pre-commit.py
+echo "Installation..."
 sudo chmod +x pre-commit.py
+echo "The installation was successful!"
 mv pre-commit.py .git/hooks/pre-commit
+echo "The pre-commit script has been moved to .git/hooks/pre-commit. Enjoy using it!"
