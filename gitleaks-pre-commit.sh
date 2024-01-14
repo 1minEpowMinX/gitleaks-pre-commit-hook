3732f51da478f216860e967ca7341561cc54d317
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2024-01-14 22:11:51 +0000

    edit msg

--- gitleaks-pre-commit.sh
+++ gitleaks-pre-commit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -sSL https://raw.githubusercontent.com/1minEpowMinX/gitleaks-pre-commit-hook/main/pre-commmit.py -o pre-commit.py
-sudo chmod +x pre-commit.py
-mv pre-commit.py .git/hooks/pre-commit
-
 # Display success messages
 printf "Installation...\n"
 printf "The installation was successful!\n"
 printf "The pre-commit script has been moved to .git/hooks/pre-commit. Enjoy using it!\n"
+
+curl -sSL https://raw.githubusercontent.com/1minEpowMinX/gitleaks-pre-commit-hook/main/pre-commmit.py -o pre-commit.py
+sudo chmod +x pre-commit.py
+mv pre-commit.py .git/hooks/pre-commit
